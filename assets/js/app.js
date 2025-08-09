ed1c53bfd179926e10e5fb7cd9bc900a59f31fb7
rishinduyohan <rishinduyohan@gmail.com>
2025-08-09 12:48:06 +0530

    Update nav bar toggle icon

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,24 +24,24 @@ if (navbarContainer) {
     navItem.innerHTML = `
         <div class="container" >
             <a class="navbar-brand" href="#"><img src="assets/images/icons8-chatgpt.svg" alt="Logo"
-                    style="width:40px;"></a>
+                style="width:40px;"></a>
 
             <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                <span class="navbar-toggler-icon "></span>
+            <span class="navbar-toggler-icon" style="filter: invert(1);"></span>
             </button>
 
             <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                <ul class="navbar-nav align-items-center">
-                    <li class="nav-item"><a class="nav-color" href="index.html">Home</a></li>
-                    <li class="nav-item"><a class="nav-color" href="about.html">About me</a></li>
-                    <li class="nav-item"><a class="nav-color " href="#">Skills</a></li>
-                    <li class="nav-item"><a class="nav-color " href="#">Projects</a></li>
-                    <li class="nav-item">
-                        <a class="btn btn-light rounded-pill px-3" href="#">CONTACT ME</a>
-                    </li>
-                </ul>
+            <ul class="navbar-nav align-items-center">
+                <li class="nav-item"><a class="nav-color" href="index.html">Home</a></li>
+                <li class="nav-item"><a class="nav-color" href="about.html">About me</a></li>
+                <li class="nav-item"><a class="nav-color " href="#">Skills</a></li>
+                <li class="nav-item"><a class="nav-color " href="#">Projects</a></li>
+                <li class="nav-item">
+                <a class="btn btn-light rounded-pill px-3" href="#">CONTACT ME</a>
+                </li>
+            </ul>
             </div>
         </div>
-    `;
+        `;
     navbarContainer.appendChild(navItem);
 }
