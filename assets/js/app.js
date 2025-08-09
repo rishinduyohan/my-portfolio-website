69430df2178f90d6d7c607e585973c6987e2e94b
rishinduyohan <rishinduyohan@gmail.com>
2025-08-09 12:33:51 +0530

    Add hero section in About page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,28 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const items = [
 
+    const items = [
+        { id: 1, title: 'Freelance Writer', description: ' I’m working as a writer in Medium.com', img: 'assets/images/medium.png' },
+        { id: 1, title: 'Software Engineering Student', description: ' I’m a software engineering student at iCET', img: 'assets/images/green.png' },
+        { id: 1, title: 'Designer', description: ' I’m a Graphic Designer of CV&B', img: 'assets/images/CV&B.png' },
     ];
 
-    const container = document.createElement('div');
-    container.id = 'sample-items';
-
-    items.forEach(item => {
-        const itemDiv = document.createElement('div');
-        itemDiv.className = 'portfolio-item';
-        itemDiv.innerHTML = `
-            <h3>${item.title}</h3>
-            <p>${item.description}</p>
+const container = document.getElementById('');
+if (container) {
+    items.forEach(plants => {
+        const card = document.createElement('div');
+        card.className = 'col-sm-6 col-md-4 col-lg-4';
+        card.innerHTML = `
+           
         `;
-        container.appendChild(itemDiv);
+        container.appendChild(card);
     });
-
-    document.body.appendChild(container);
-});
+}
 
 const navbarContainer = document.getElementById('navbar');
 if (navbarContainer) {
     const navItem = document.createElement('nav');
-    navItem.className = 'navbar navbar-expand-lg navbar-dark bg-transparent py-4 fixed';
+    navItem.className = 'navbar navbar-expand-lg navbar-dark bg-transparent py-4';
     navItem.innerHTML = `
-        <div class="container">
+        <div class="container" >
             <a class="navbar-brand" href="#"><img src="assets/images/icons8-chatgpt.svg" alt="Logo"
                     style="width:40px;"></a>
 
@@ -34,7 +32,8 @@ if (navbarContainer) {
 
             <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul class="navbar-nav align-items-center">
-                    <li class="nav-item"><a class="nav-color" href="#">About me</a></li>
+                    <li class="nav-item"><a class="nav-color" href="index.html">Home</a></li>
+                    <li class="nav-item"><a class="nav-color" href="about.html">About me</a></li>
                     <li class="nav-item"><a class="nav-color " href="#">Skills</a></li>
                     <li class="nav-item"><a class="nav-color " href="#">Projects</a></li>
                     <li class="nav-item">
